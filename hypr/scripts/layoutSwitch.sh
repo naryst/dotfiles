1baa5dd7c2db585248b7ebca24019e56bd15144d
naryst <nikitasergeev692@gmail.com>
2023-09-10 19:11:37 +0300

    first commit

---
+++ hypr/scripts/layoutSwitch.sh
@@ -0,0 +1,9 @@
+# switch-lang
+hyprctl \
+  --batch "$(
+    hyprctl devices -j |
+      jq -r '.keyboards[] | .name' |
+      while IFS= read -r keyboard; do
+        printf '%s %s %s;' 'switchxkblayout' "${keyboard}" 'next'
+      done
+  )"
